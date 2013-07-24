98f6439cde85413baa2b97091acb738e2698bad6
Joshua Bangel <falornan@gmail.com>
2013-07-23 20:41:07 -0500

    Added titles to home and about pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:basetitle) { 'Oz | ' }
+
   describe "Home" do
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{basetitle}Home")
+    end
+
     it "includes an input box" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
@@ -10,6 +17,11 @@ describe "StaticPages" do
   end
 
   describe "About" do
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{basetitle}About")
+    end
+
     it "incluces Oz" do
       visit '/static_pages/about'
       expect(page).to have_content('Oz')
